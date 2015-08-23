1f762550c7d7d7e9313469efd443ed9b139f0175
System Administrator <root@Colins-MacBook-Pro.local>
2015-08-23 14:26:17 -0400

    Initial Commit!

---
+++ src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Player player = new Player("file:///temp/file.mp4");
+        Scene scene = new Scene(player, 720, 480, Color.BLACK);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
